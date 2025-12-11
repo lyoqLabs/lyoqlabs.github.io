be9a693d9dc4b73180b1233b85521ec49d27fa23
LYOQLABS <projectvexaone@gmail.com>
2025-12-11 11:17:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+/* SIDEBAR TOGGLE */
+
+const toggleBtn = document.getElementById("toggle-btn");
+const sidebar = document.getElementById("sidebar");
+
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("collapsed");
+});
+
+
+/* PARTICLE BACKGROUND */
+
 const canvas = document.getElementById("bg-canvas");
 const ctx = canvas.getContext("2d");
 
@@ -6,18 +18,18 @@ canvas.height = window.innerHeight;
 
 let particles = [];
 
-for (let i = 0; i < 70; i++) {
+for (let i = 0; i < 80; i++) {
   particles.push({
     x: Math.random() * canvas.width,
     y: Math.random() * canvas.height,
-    size: 2 + Math.random() * 3,
-    speedX: (Math.random() - 0.5) * 0.4,
-    speedY: (Math.random() - 0.5) * 0.4
+    size: 1 + Math.random() * 3,
+    speedX: (Math.random() - 0.5) * 0.7,
+    speedY: (Math.random() - 0.5) * 0.7
   });
 }
 
 function animate() {
-  ctx.fillStyle = "rgba(0,0,0,0.25)";
+  ctx.fillStyle = "rgba(0,0,0,0.2)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   particles.forEach(p => {
@@ -27,7 +39,10 @@ function animate() {
     if (p.x < 0 || p.x > canvas.width) p.speedX *= -1;
     if (p.y < 0 || p.y > canvas.height) p.speedY *= -1;
 
-    ctx.fillStyle = "white";
+    ctx.shadowBlur = 10;
+    ctx.shadowColor = "#00eaff";
+    ctx.fillStyle = "#00eaff";
+
     ctx.beginPath();
     ctx.arc(p.x, p.y, p.size, 0, Math.PI * 2);
     ctx.fill();
